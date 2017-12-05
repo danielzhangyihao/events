8015562dc28e54e6ddb713323f8f8aac609bb935
daniel zhang <zhangyihao1009@gmail.com>
2017-12-04 23:45:54 -0800

    lower case eventdate sort

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -9,7 +9,7 @@ class EventsController < ApplicationController
 
   def eventsJson
     offset = params[:offset].to_i
-    @events = Event.where('eventDate >= ?', Date.today).order(:eventDate).offset(offset).limit(10)
+    @events = Event.where('eventdate >= ?', Date.today).order(:eventdate).offset(offset).limit(10)
     totalCount = @events.count
     if offset >= totalCount
       offset = offset + 10
